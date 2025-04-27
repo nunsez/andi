ca6e108d1d01b41f6cf9f12d71b696738f0a2aa9
Alexander Mandrikov <mandrikov@pm.me>
2025-04-27 17:14:57 +0700

    add build.v

---
+++ build.v
@@ -0,0 +1,20 @@
+module main
+
+import os
+
+fn main() {
+	sys := arguments()[1] or { os.user_os() }
+	out := match sys {
+		'linux' {
+			'andi'
+		}
+		'windows' {
+			'andi.exe'
+		}
+		else {
+			eprintln('invalid os: ${sys}')
+			return
+		}
+	}
+	os.system('v ./src/main.v -os $sys -o $out')
+}
